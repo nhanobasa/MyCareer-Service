e9474dea5f3ac8ed22a686a459a5cfb18498a3c8
nhantd <tranducnhan@admicro.vn>
2021-06-13 04:52:38 +0700

    thêm hàm tính số lượt like cho job và lọc cho getAllJobs

--- src/main/java/vn/nhantd/mycareer/service/JobServiceImpl.java
+++ src/main/java/vn/nhantd/mycareer/service/JobServiceImpl.java
@@ -6,12 +6,17 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+import vn.nhantd.mycareer.model.Transaction;
+import vn.nhantd.mycareer.model.employeer.Employer;
 import vn.nhantd.mycareer.model.job.Job;
 import vn.nhantd.mycareer.model.user.User;
 import vn.nhantd.mycareer.model.user.User_career_goals;
 import vn.nhantd.mycareer.repository.JobRepository;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class JobServiceImpl implements JobService {
@@ -83,6 +88,44 @@ public class JobServiceImpl implements JobService {
         }
         query = query.addCriteria(Criteria.where("status").is("active"));
 
-        return mongoTemplate.find(query, Job.class);
+        List<Job> jobList = mongoTemplate.find(query, Job.class);
+
+        jobList.sort(new Comparator<Job>() {
+            @Override
+            public int compare(Job o1, Job o2) {
+                return calculateTotalLike(o2).compareTo(calculateTotalLike(o1));
+            }
+        });
+
+        return jobList;
+    }
+
+    private Long calculateTotalLike(Job job) {
+        // TÍNH TOÁN SỐ LƯỢT QUAN TÂM ĐẾN NHÀ TUYỂN DỤNG
+        //lọc lấy lượt yêu thích ??? lỗi chỗ này ???
+        List<Transaction> likeList = job.getTransactions().stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        //sort by dt DESC
+        likeList.sort(new Comparator<Transaction>() {
+            @Override
+            public int compare(Transaction o1, Transaction o2) {
+                return o2.getDt().compareTo(o1.getDt());
+            }
+        });
+
+        // distinct theo user_id
+        HashSet<Object> seen = new HashSet<>();
+
+        // tính số lượt quan tâm
+        long totalLike = 0;
+        likeList.removeIf(e -> !seen.add(e.getUser_id()));
+        for (Transaction transaction : likeList) {
+            if (transaction.getTransaction_code().equals("like")) {
+                totalLike += 1;
+            }
+        }
+        return totalLike;
     }
 }
